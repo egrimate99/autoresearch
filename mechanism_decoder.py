5e9da342dc1982cf17af3ae1de698a5e0a7d5039
egrimate99 <egrimate99@gmail.com>
2026-05-24 04:38:49 +0100

    continue target-union penalty tuning

--- mechanism_decoder.py
+++ mechanism_decoder.py
@@ -39,7 +39,7 @@ class MechanismDecoder:
             device=table_logits.device,
         )
         masked_logits = table_logits.clone()
-        masked_logits[:, ~allowed] = masked_logits[:, ~allowed] - 2.6
+        masked_logits[:, ~allowed] = masked_logits[:, ~allowed] - 2.7
         target_counts = torch.as_tensor(
             scr.target_mask.sum(axis=0),
             dtype=masked_logits.dtype,
